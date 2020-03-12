66695a681f1ac72639d0ac8bc57e8bce03c451f5
RAB_X69 <jayednahian@yahoo.com>
2020-03-13 00:12:36 +0600

    Update function_Max_three_numbers.py

--- function_Max_three_numbers.py
+++ function_Max_three_numbers.py
@@ -1,10 +1,14 @@
-def max_of_two( x, y ):
-    if x > y:
+def max_of_two( x, y,z ):
+    if x>y>z:
         return print("big value is: ",x)
-    return print("big value is: ",y)
+    elif y>x>z:
+        return print("big value is: ",y)
+    else:
+        return print("big values is: ",z)
 
-a = int(input("value one: "))
-b= int(input("value two: "))
+a=int(input("value one: "))
+b=int(input("value two: "))
+c=int(input("value three: "))
 
 
-max_of_two(a,b)
+max_of_two(a,b,c)
