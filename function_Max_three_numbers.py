9cc41c1099cfc29d8a7922dba033ec5a0b43f5fa
RAB_X69 <jayednahian@yahoo.com>
2020-03-13 00:11:26 +0600

    Add files via upload

---
+++ function_Max_three_numbers.py
@@ -0,0 +1,10 @@
+def max_of_two( x, y ):
+    if x > y:
+        return print("big value is: ",x)
+    return print("big value is: ",y)
+
+a = int(input("value one: "))
+b= int(input("value two: "))
+
+
+max_of_two(a,b)
